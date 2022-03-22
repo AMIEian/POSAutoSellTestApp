324b4d5e2180ca7ebf1778c652d0e51e84bfbf3e
AMIEian <nitin4july@gmail.com>
2022-03-22 17:28:46 +0530

    Add project files.

---
+++ TestApp/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TagidPOS;
+
+namespace TestApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string barcodes = textBox1.Text;
+            barcodes = barcodes.Replace("\r\n", ",");
+            TagidPOS.TagidPOSHelper.PutBilledBarcodes(barcodes);
+        }
+    }
+}
